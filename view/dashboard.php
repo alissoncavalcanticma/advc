9e8c70637a107bb47e942f15e24ed4278e6fae51
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2020-07-19 16:21:28 +0000

    inc nome e foto do usuario logado em menu.php e start de session em dashboard.php, inc da pasta users em /assets/img com fotos do usuario logado

--- view/dashboard.php
+++ view/dashboard.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+
 <!-- header start -->
 <?php include_once 'header.php'; ?>
 <!--header end-->
--- view/menu.php
+++ view/menu.php
@@ -7,8 +7,8 @@ MAIN SIDEBAR MENU
       <div id="sidebar" class="nav-collapse ">
         <!-- sidebar menu start-->
         <ul class="sidebar-menu" id="nav-accordion">
-          <p class="centered"><a href="profile.html"><img src="../assets/img/ui-sam.jpg" class="img-circle" width="80"></a></p>
-          <h5 class="centered">Sam Soffes</h5>
+          <p class="centered"><a href="profile.html"><img src="../assets/img/users/<?= strtolower($_SESSION['logon']) ?>.jpg" class="img-circle" width="80"></a></p>
+          <h5 class="centered"><?php echo $_SESSION['logon']; ?></h5>
           <li class="mt">
             <a class="active" href="index.html">
               <i class="fa fa-dashboard"></i>
